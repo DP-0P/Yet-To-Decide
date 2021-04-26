c2ba8c4a5d30b78adec40979cff9ed54f6510c8d
DP_OP <deepakpattnayak2013@gmail.com>
2021-04-26 22:51:30 +0530

    new index html

--- Login/index.php
+++ Login/index.php
@@ -1,156 +1,192 @@
-<?php 
+<?php
 session_start();
 
-	include("connection.php");
-	include("functions.php");
+include("connection.php");
+include("functions.php");
 
 
-	$user_data = check_login($con);
+$user_data = check_login($con);
 ?>
 
 <!DOCTYPE html>
-<html>
-<head>  <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, intial-scale=1.0">
-    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="styless.css">
-    <title>Notes Exchange System</title>
-	
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Note Exchange System</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" href="indexLight.css">
 </head>
 
-	<body>
-    <a href="#" class="scrolltop " id="scroll-top">
-        <i class='bx bx-chevron-up scrolltop-icon'></i>
-    </a>
-    <header class="header1" id="header">
-        <nav class="nav 1-container">
-            <img class="logo" src="photos/nes.png">
-            <a href="#" class="nav-logo">Notes Exchange System
-
-            </a>
-             Hello, <?php echo $user_data['user_name']; ?>
-
-            <div class="nav-menu" id="nav-menu">
-                <ul class="nav-list">
-                    <li class="nav-items"><a href="#home" class="nav-link active-link">Home</a></li>
-                    <li class="nav-items"><a href="#about us" class="nav-link">About us</a></li>
-                    <li class="nav-items"><a href="#upload" class="nav-link">upload/Download</a></li>
-                    <li class="nav-items"><a href="#contact" class="nav-link">Contact Us</a></li>
-                     <li class="nav-items"><a href="logout.php" class="nav-link">Log Out</a></li>
-                </ul>
+<body>
+    <div class="main-wrapper">
+        <div class="nav-background">
+            <div class="mobile-logo">
+                <img src="./icons/logo.svg" alt="">
             </div>
-            <div class="nav-toggle" id="nav-toggle">
-                <i class='bx bx-menu'></i>
-            </div>
-        </nav>
-
-    </header>
-    <main class="main1">
-        <section class="home" id="home">
-            <div class="home-container bd-container bd-grid">
-                <div class="home-data">
-                    <h1 class="home-title">Notes Exchange System</h1>
-                    <h2 class="home-sub">The Only Thing We're Serious About <br> Is Future.</h2>
-                    <a href="#"
-                        class="button" target="_blank">Available Books</a>
-                </div>
-                <img src="photos/homebook.png" alt="" class="home-img">
-            </div>
-
-        </section>
-
-        <section class="about section bd-container" id="about us">
-            <div class="about-container bd-grid">
-                <div class="about-data">
-                    <span class="section-sub about-initial">About Us</span>
-                    <h2 class="section-title about-initial">The Best We Do is Creating <br> Enviroment And Platform To study</h2>
-                    <p class="about-description">We build and developed the place where you can find all the neccessary items like pdf,notes,books and many more. We help to make you thrive in your path to future.</p>
-                    <a href="About Us.html" class="button"
-                        target="_blank">About Us</a>
-
-                </div>
-                <img src="photos/book2.png" alt="" class="about-img">
-            </div>
-             <section class="about section bd-container" id="upload">
-            <div class="about-container bd-grid">
-                <div class="about-data">
-                    <span class="section-sub about-initial">upload</span>
-                  
-                    <a href="#" class="button"
-                        target="_blank">upload/Download</a>
-
+            <div class="mobile-nav">
+                <div class="nav-top">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About us</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact us</a>
+                        </li>
+                        <li>
+                            <a href="#">Log Out</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            
-
-       
-       
-            <section class="contact section bd-container" id="contact">
-                <div class="contact-container bd-grid" id="tt">
-                    <div class="contact-data">
-                        <span class="section-sub contact-initial">Let's Talk</span>
-                        <h2 class="section-title contact-initial">Contact Us</h2>
-                        <p class="contact-description">For any queries and problem facing in this websites contact us. We
-                            are available 24*7.</p>
-                    </div>
-                    <div class="contact-button">
-                        <a href="mailto:sririjuldas@gmail.com,rjldas00@gmail.com,deepakpattnayak2013@gmail.com,rajprusty99@gmail.com" class="button">Reach Us Now</a>
-                    </div>
-                </div>
-
-            </section>
-            <footer class="footer section bd-container">
-                <div class="footer-container bd-grid">
-                    <div class="footer-content">
-                        <a href="#" class="footer-logo">Notes Exchange System</a>
-                        <span class="footer-description">Platform for notes exchange</span>
-                        <div>
-                            <a href="https://www.facebook.com/" class="footer-social" target="_blank"><i
-                                    class='bx bxl-facebook'></i></a>
-                            <a href="https://www.instagram.com/" class="footer-social" target="_blank"><i
-                                    class='bx bxl-instagram'></i></a>
-                            <a href="https://twitter.com/login" class="footer-social" target="_blank"><i
-                                    class='bx bxl-twitter'></i></a>
+        </div>
+        <div class="site-content-wrapper">
+            <div class="nav-trigger">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart">
+                    <line x1="12" y1="20" x2="12" y2="10" />
+                    <line x1="18" y1="20" x2="18" y2="4" />
+                    <line x1="6" y1="20" x2="6" y2="16" />
+                </svg>
+            </div>
+            <div class="site-content">
+                <header class="topbar">
+                    <div class="container flex justify-between items-center">
+                        <div class="icons">
+                            <a href="#"><img src="./icons/facebook.svg" alt=""></a>
+                            <a href="#"><img src="./icons/twitter.svg" alt=""></a>
+                            <a href="#"><img src="./icons/google.svg" alt=""></a>
+                            <a href="#"><img src="./icons/instagram.svg" alt=""></a>
+                            <a href="#"><img src="./icons/search.svg" alt=""></a>
+                        </div>
+                        <div class="auth flex items-center">
+                            <div>
+                                <img src="./icons/user-icon.svg" alt="">
+                                <a href="logout.php">Log Out</a>
+                            </div>
                         </div>
                     </div>
-                   
-                    <div class="footer-content">
-                        <h3 class="footer-title">Information</h3>
-                        <ul>
-                            <li><a href="tel:+918598000020" class="footer-link">Call Us</a></li>
-                            <li><a href="mailto:sririjuldas@gmail.com,rjldas00@gmail.com,deepakpattnayak2013@gmail.com,rajprusty99@gmail.com" class="footer-link">Mail
-                                    Us</a></li>
-                            <li><a href="file:///C:/Users/Rijul/OneDrive/Desktop/food%20website/Privacy%20Policy.html"
-                                    class="footer-link" target="_blank">Privacy Policy</a></li>
+                </header>
 
-                        </ul>
+                <nav>
+                    <div class="top">
+                        <div class="container flex justify-between">
+                            <div class="contact flex items-center">
+                                <img src="./icons/phone.svg" alt="">
+                                <div>
+                                    <h5>Call US: (+84) 123 456 789</h5>
+                                    <h6>E-mail : support@freshmeal.com</h6>
+                                </div>
+                            </div>
+                            <div class="branding">
+                                <img src="./icons/logo.svg" alt="">
+                            </div>
+                            <div class="time flex items-center">
+                                <img src="./icons/clock.svg" alt="">
+                                <div>
+                                    <h5>Working Hours:</h5>
+                                    <h6>Mon - Sat (69.00am - 12.00am)</h6>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="footer-content">
-                        <h3 class="footer-title">Address</h3>
-                        <ul>
-                            
-                            <li>Odisha</li>
-                            <li>Bhubaneswar</li>
-                            <li>sririjuldas@gmail.com,deepakpattnayak2013@gmail.com,rajprusty99@gmail.com</li>
-                        </ul>
+                    <div class="navbar magic-shadow">
+                        <div class="container flex justify-center">
+                            <a href="#">Home</a>
+                            <a href="#">About us</a>
+                            <a href="#">Products</a>
+                            <a href="#">Contact us</a>
+                        </div>
                     </div>
+                </nav>
 
-                </div>
-                <p class="footer-copy">&#169; 2020 NES. All Rights Reserved.</p>
+                <header class="hero flex items-center">
+                    <div class="container">
+                        <div class="welcome flex items-center">
+                            <span>Welcome to</span>
+                            <img src="./icons/logo-2.svg" alt="">
+                        </div>
+                        <h1>The World Best <span>Shoping</span> Website</h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the
+                            industry's standard dummy.</p>
+                        <div>
+                            <button class="btn btn-primary">Read More</button>
+                            <button class="btn btn-secondary">Shop Now</button>
+                        </div>
+                        <div class="hero-image">
+                            <img src="./images/straw.png" alt="">
+                        </div>
+                    </div>
+                </header>
 
-            </footer>
+               
+                
 
-            <!-- scroll revel -->
-            <script src="https://unpkg.com/scrollreveal"></script>
-            <script src="mains.js"></script>
+                <footer>
+                    <div class="container">
+                        <div class="box">
+                            <h3>About us</h3>
+                            <p>It was popularised in the 1960 with the release of Latest sheets containing Lorem Ipsum
+                                passage.</p>
+                            <button class="btn btn-secondary">Read More</button>
+                        </div>
+                        
+                        <div class="box">
+                            <h3>Follow Us</h3>
+                            <div>
+                                <ul>
+                                    <li>
+                                        <a href="https://www.facebook.com/codersgyan">
+                                            <img src="./icons/facebook.svg" alt="">
+                                            <span>Facebook</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://twitter.com/CoderGyan">
+                                            <img src="./icons/twitter.svg" alt="">
+                                            <span>Twitter</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <img src="./icons/google.svg" alt="">
+                                            <span>Google +</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.instagram.com/codersgyan/">
+                                            <img src="./icons/instagram.svg" alt="">
+                                            <span>Instagram</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </footer>
+                <footer class="copyright">
+                    <div>
+                        Copyright © 2020 .All rights reserved by <a href="https://www.instagram.com/codersgyan/">Coder's Gyan</a>.
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </div>
 
 
 
 
 
-	
-	
-	
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="js/app.js"></script>
 </body>
+
 </html>
